1bf9b234de20603c52478b53d85169fd51c1f8b6
riyad1234 <rh3510594@gmail.com>
2023-09-10 15:39:53 +0600

    first commit

---
+++ Index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const catagories = require('./data/catagoris.json');
+const news = require('./data/news.json');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Dragon is running!')
+});
+
+app.get('/catagories', (req, res) => {
+  res.send(catagories)
+});
+app.get('/news', (req, res) => {
+  res.send(news)
+});
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+});
+
+app.get('/catagories/:id', (req, res) => {
+  const id =parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news)
+  }
+  else {
+    const catagoryNews =news.filter(n=>parseInt(n.category_id)===id);
+    res.send(catagoryNews)
+  }
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
